9b4f215df2cfba848caf21f49977f8f241d08457
stanyzra <stanyhelberth@gmail.com>
2024-07-18 11:09:03 -0300

    chore: add version command

---
+++ cmd/version.go
@@ -0,0 +1,23 @@
+/*
+Copyright © 2024 Stany Helberty stanyhelberth@gmail.com
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show the version number of env-manager-v2",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("0.1.0")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
