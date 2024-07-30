ffac66945d21d5552d22ae8905447f641eb33807
stanyzra <stanyhelberth@gmail.com>
2024-07-30 14:40:46 +0000

    chore: brew formula update for homebrew-env-manager-v2 v0.5.0

--- Formula/homebrew-env-manager-v2.rb
+++ Formula/homebrew-env-manager-v2.rb
@@ -5,8 +5,8 @@
 class HomebrewEnvManagerV2 < Formula
   desc "Environment variable manager for aws amplify, dgo apps and oci object storage,"
   homepage "https://github.com/stanyzra/homebrew-env-manager-v2"
-  url "https://github.com/stanyzra/homebrew-env-manager-v2/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "065e6a10c4cddb5aa0791c5af2ac7626049ddfc958b9e70c89c30a3b0b7d5ef4"
+  url "https://github.com/stanyzra/homebrew-env-manager-v2/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "07bf14cbc4aedba0372b3727424f4e7726941b3531583980e1c07b439ef25968"
   license "Apache-2.0"
 
   depends_on "go" => :build
@@ -16,6 +16,6 @@ class HomebrewEnvManagerV2 < Formula
   end
 
   test do
-    assert_match "0.4.0", shell_output("#{bin}/env-manager-v2 --version")
+    assert_match "0.5.0", shell_output("#{bin}/env-manager-v2 --version")
   end
 end
